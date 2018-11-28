bb4446d53637ea0ca54acba0103d751e0f46a84a
Secretaria de Salud <fco_arango@hotmail.com>
2018-11-28 13:16:21 -0600

    se agrego boton actualizar lista finalizado

--- js/app.js
+++ js/app.js
@@ -6,5 +6,13 @@ btnActualizar.addEventListener('click', actualizar);
 
 function actualizar()
 {
-  alert('actualizar el listado');
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+      if (this.readyState == 4 && this.status == 200){
+        alert(this.responseText);
+      }
+  };
+  xhttp.open("GET", "http://nyc.pixan.io/ajax/public/api/students", true);
+  xhttp.send();
+
 }
