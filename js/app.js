0449bcb263d56a5b71e738f79e635001276c0c7f
Secretaria de Salud <fco_arango@hotmail.com>
2018-11-28 12:57:45 -0600

    se agrego boton actualizar lista

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Obtener la referencia al elemento
+var btnActualizar = document.getElementById('btnActualizar')
+
+//Agregar un event listener para cada click
+btnActualizar.addEventListener('click', actualizar);
+
+function actualizar()
+{
+  alert('actualizar el listado');
+}
